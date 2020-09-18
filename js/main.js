403c353557a66f952365a3870bc0055aaa463097
Javohir Shodiyev <68178242+vestop@users.noreply.github.com>
2020-09-18 18:29:59 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(".preview__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev arrow-left"><img src="images/arrow-left.svg" alt=""></img></button>',
+    nextArrow:
+      '<button type="button" class="slick-next arrow-right"><img src="images/arrow-right.svg" alt=""></img></button>',
+    fade: true,
+  });
+});
